e18a5fa59d3cd410a8a6d178c20f2676749b03b4
Sarthak Mittal <sarmittal@gmail.com>
2024-08-22 12:02:19 +0530

    bug  removed

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 import 'dotenv/config'
 
 export const connectDB = async()=>{
-    await mongoose.connect(`${process.env.MONGODB_URI}`).then((success)=>{
+    await mongoose.connect(`${process.env.MONGODB_URI}`,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 30000,
+    }).then((success)=>{
         console.log("Successfully connected");
     }).catch((error)=>{
         console.log(`Find error while connecting with mongoDB with error = ${error}`);
